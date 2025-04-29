03ae75df457cef01515a9775b834614caa13ceda
HavenGo <havengoitom2@gmail.com>
2025-04-29 06:34:14 +0300

    fixed some issues in settings

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import (
-    RegisterView, LogoutView
+    RegisterView, LogoutView, UserProfileView
 )
 
 urlpatterns = [
-    path('auth/register/', RegisterView.as_view(), name='register'),
-    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('auth/logout/', LogoutView.as_view(), name='logout')
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('auth/profile/<pk>/', UserProfileView.as_view(), name='user-profile')
 
 ]
