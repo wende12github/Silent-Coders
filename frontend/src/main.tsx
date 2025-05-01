06b5a5ab2a7565e9fad43451e2b742c9d8a8ed69
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-05-01 01:09:08 -0700

    add leader board page code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,10 @@
-<<<<<<< HEAD
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import Leaderboard from "./Components/Leaderboard.tsx";
 
-createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>
-);
-=======
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
@@ -36,4 +24,4 @@ non ! delete (!) symbol.
 -adjust index.html main.txs to main.jsx
 
  */
->>>>>>> aabb6135a19ce77609e0fcbbd66bb6efff8fddec
+
