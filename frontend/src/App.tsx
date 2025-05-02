82a5b780d0070ec0c370a561194ddb1671055206
mahi7000 <mahlet7000@gmail.com>
2025-05-02 11:48:31 +0300

    Add landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,21 @@
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
+// src/main.jsx or App.jsx
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, far, fab) // Add all icons or specific ones
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 
 function App() {
     return (
         <BrowserRouter>
-            <Header />
+            {/* <Header /> */}
             <AppRoutes />
-            <Footer />
+            {/* <Footer /> */}
         </BrowserRouter>
     );
 }
