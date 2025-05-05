a8c9235430e22dc4e5f1f6906ba29ad01876de94
Emnet19 <emnbef456@gmail.com>
2025-05-05 08:39:11 +0300

    LeaderboardApiCorrection

--- backend/leaderboard/views.py
+++ backend/leaderboard/views.py
@@ -10,6 +10,10 @@ class LeaderboardViewSet(viewsets.ReadOnlyModelViewSet):
     ordering = ['-total_hours_given']
 
     def get_queryset(self):
+
+        if self.action == 'retrieve':
+        return UserStats.objects.all() 
+        
         sort_by = self.request.query_params.get('sort_by', 'given')  # default: given
         top_n = int(self.request.query_params.get('top', 10))
 
