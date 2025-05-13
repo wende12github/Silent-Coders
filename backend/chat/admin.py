3a857a0f4f1e39f89bc11abd65bae846592e2601
Wendmagegn Tajura <149209819+wende12github@users.noreply.github.com>
2025-05-13 10:19:56 +0300

    comment unused models

--- backend/chat/admin.py
+++ backend/chat/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import ChatMessage
+# from .models import ChatMessage
 
-@admin.register(ChatMessage)
-class ChatMessageAdmin(admin.ModelAdmin):
+# @admin.register(ChatMessage)
+# class ChatMessageAdmin(admin.ModelAdmin):
     # list_display = ('user', 'room', 'message_type', 'created_at', 'is_read')
-    search_fields = ('user__username', 'room__name', 'message')
+    # search_fields = ('user__username', 'room__name', 'message')
     # list_filter = ('message_type', 'is_read', 'created_at')
