95818cd2484919bee9e277d72be848a7113eb54e
Wendmagegn Tajura <149209819+wende12github@users.noreply.github.com>
2025-05-13 10:13:45 +0300

    import from django.utils.timezone import now

--- backend/wallet/models.py
+++ backend/wallet/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from decimal import Decimal
 from django.core.validators import MinValueValidator
+from django.utils.timezone import now
 
 User = settings.AUTH_USER_MODEL
 
