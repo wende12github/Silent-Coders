701fb6edea851fe7e2f9376e9af407240512763b
Wendmagegn Tajura <149209819+wende12github@users.noreply.github.com>
2025-05-13 10:31:43 +0300

    INTGRATE frontend with backend

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,3 +1,4 @@
 // export const API_BASE_URL = 'http://127.0.0.1:8000';
-export const API_BASE_URL = 'https://timebank-39ps.onrender.com';
+// export const API_BASE_URL = 'https://timebank-39ps.onrender.com';
+export const  API_BASE_URL= 'https://time-bank-pro.onrender.com';
 export const REFRESH_TOKEN_STORAGE_KEY = 'refreshToken';
