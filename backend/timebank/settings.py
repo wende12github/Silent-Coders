35a790d6c0e206930130ae54d9bd082778daed82
Wendmagegn Tajura <149209819+wende12github@users.noreply.github.com>
2025-05-13 10:00:39 +0300

    setup Radis url

--- backend/timebank/settings.py
+++ backend/timebank/settings.py
@@ -257,12 +257,13 @@ SWAGGER_SETTINGS = {
     },
 }
 
+REDIS_URL = os.environ.get("REDIS_URL", "redis://localhost:6379")
 # for actual deploayment
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('127.0.0.1', 6379)], # Redis server
+            'hosts': [REDIS_URL], # Redis server
         },
     },
 }
