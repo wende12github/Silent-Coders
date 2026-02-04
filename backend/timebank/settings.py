44483118d1ea0932af1d3cd6abafb497740c2068
wende12github <wendmagegn9412@gmail.com>
2026-02-04 13:06:25 +0300

    Added a new module backend/utils/api_exceptions.py providing custom_exception_handler

--- backend/timebank/settings.py
+++ backend/timebank/settings.py
@@ -215,6 +215,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'EXCEPTION_HANDLER': 'utils.api_exceptions.custom_exception_handler',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': [
